ea51449440101a9ec38d9e4dc896af9cabb74f91
KumarManish90 <146661359+KumarManish90@users.noreply.github.com>
2026-08-24 22:36:43 +0530

    refactor(frontend): route compatibility layers through stabilization boundary

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,7 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import MediaUploadLayer from "./MediaUploadLayer.jsx";
-import LiveMediaHydrator from "./LiveMediaHydrator.jsx";
-import PhaseCPostLayer from "./PhaseCPostLayer.jsx";
-import PhaseCPaginationLayer from "./PhaseCPaginationLayer.jsx";
-import PhaseDReelsLayer from "./PhaseDReelsLayer.jsx";
-import PhaseEStoriesLayer from "./PhaseEStoriesLayer.jsx";
-import PhaseFProfileLayer from "./PhaseFProfileLayer.jsx";
-import CreateMediaShortcutLayer from "./CreateMediaShortcutLayer.jsx";
+import StabilizationLayers from "./StabilizationLayers.jsx";
 import { installThemeBackgroundGuard } from "./themeBackgroundGuard.js";
 import "./responsive.css";
 import "./responsive-edge.css";
@@ -22,13 +15,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeBackgroundGuard />
     <App />
-    <MediaUploadLayer />
-    <LiveMediaHydrator />
-    <PhaseCPostLayer />
-    <PhaseCPaginationLayer />
-    <PhaseDReelsLayer />
-    <PhaseEStoriesLayer />
-    <PhaseFProfileLayer />
-    <CreateMediaShortcutLayer />
+    <StabilizationLayers />
   </React.StrictMode>
 );
