8e4575844410a92d1d8f024f175ee3a0815379b8
KumarManish90 <146661359+KumarManish90@users.noreply.github.com>
2026-08-24 16:54:28 +0530

    feat(media): mount real media upload layer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import MediaUploadLayer from "./MediaUploadLayer.jsx";
 import "./responsive.css";
 import "./responsive-edge.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <MediaUploadLayer />
   </React.StrictMode>
 );
