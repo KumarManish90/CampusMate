32a0f4994407ba3ab201d80aacaa39f64f21cb88
KumarManish90 <146661359+KumarManish90@users.noreply.github.com>
2026-08-24 14:55:11 +0530

    refactor(data): complete shared data barrel exports

--- frontend/src/data/index.js
+++ frontend/src/data/index.js
@@ -1,3 +1,6 @@
 export * from "./campusData";
 export * from "./collegeUtils";
+export * from "./filterOptions";
 export * from "./navigation";
+export * from "./socialConstants";
+export * from "./students";
