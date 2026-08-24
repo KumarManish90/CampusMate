660063e82775bbba1eb13a949636bd21834c7fa5
KumarManish90 <146661359+KumarManish90@users.noreply.github.com>
2026-08-24 17:47:18 +0530

    fix mobile dark mode overscroll background

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import MediaUploadLayer from "./MediaUploadLayer.jsx";
 import LiveMediaHydrator from "./LiveMediaHydrator.jsx";
+import { installThemeBackgroundGuard } from "./themeBackgroundGuard.js";
 import "./responsive.css";
 import "./responsive-edge.css";
 
+function ThemeBackgroundGuard() {
+  useEffect(() => installThemeBackgroundGuard(), []);
+  return null;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ThemeBackgroundGuard />
     <App />
     <MediaUploadLayer />
     <LiveMediaHydrator />
