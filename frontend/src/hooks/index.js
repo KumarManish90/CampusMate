d23e52bd58487a31b9ea5945884e29609e125df9
KumarManish90 <146661359+KumarManish90@users.noreply.github.com>
2026-08-24 14:40:03 +0530

    refactor(hooks): export app navigation hook

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,6 +1,7 @@
 export * from "./useCampusMateSession";
 export * from "./useCampusMateFeed";
 export * from "./useLiveSocialFeed";
+export * from "./useAppNavigation";
 export * from "./useMediaQuery";
 export * from "./useSocialActions";
 export * from "./useThemeMode";
