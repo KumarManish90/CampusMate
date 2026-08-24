58f44fd1919df1ba0a5cc6a382ea50804accde24
KumarManish90 <146661359+KumarManish90@users.noreply.github.com>
2026-08-24 15:33:49 +0530

    feat: wire phase A responsive edge layer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./responsive.css";
+import "./responsive-edge.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
