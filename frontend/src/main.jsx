d2e3fc479a11b69b564585713452d8c6d3408a80
KumarManish90 <146661359+KumarManish90@users.noreply.github.com>
2026-08-24 18:21:32 +0530

    feat(posts): mount Phase C pagination layer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import MediaUploadLayer from "./MediaUploadLayer.jsx";
 import LiveMediaHydrator from "./LiveMediaHydrator.jsx";
 import PhaseCPostLayer from "./PhaseCPostLayer.jsx";
+import PhaseCPaginationLayer from "./PhaseCPaginationLayer.jsx";
 import { installThemeBackgroundGuard } from "./themeBackgroundGuard.js";
 import "./responsive.css";
 import "./responsive-edge.css";
@@ -20,5 +21,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <MediaUploadLayer />
     <LiveMediaHydrator />
     <PhaseCPostLayer />
+    <PhaseCPaginationLayer />
   </React.StrictMode>
 );
