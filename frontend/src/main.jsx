5802aadbc6930f9e80580d43b4ef15b3f6400a7c
KumarManish90 <146661359+KumarManish90@users.noreply.github.com>
2026-08-24 17:22:28 +0530

    fix: hydrate live post media

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import MediaUploadLayer from "./MediaUploadLayer.jsx";
+import LiveMediaHydrator from "./LiveMediaHydrator.jsx";
 import "./responsive.css";
 import "./responsive-edge.css";
 
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <MediaUploadLayer />
+    <LiveMediaHydrator />
   </React.StrictMode>
 );
